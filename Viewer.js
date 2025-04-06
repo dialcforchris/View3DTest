ecc389ab5c3b8f9e6f4ccaa87ddc6af8e4e79665
DialC <mrchrismugridge@gmail.com>
2025-04-06 21:15:57 +0100

    init

---
+++ Viewer.js
@@ -0,0 +1,7 @@
+import View3D from "@egjs/view3d";
+import "@egjs/view3d/css/view3d-bundle.min.css";
+
+const view3D = new View3D("#view3d", {
+  src: "https://github.com/KhronosGroup/glTF-Sample-Models/blob/main/2.0/ToyCar/glTF/ToyCar.gltf",
+  envmap: "https://github.com/KhronosGroup/glTF-Sample-Models/blob/main/2.0/ToyCar/glTF/ToyCar_basecolor.png",
+});
