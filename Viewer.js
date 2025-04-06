81b71cc4c0a8a4f1d1a5443047eabc1de6499cd3
DialC <mrchrismugridge@gmail.com>
2025-04-06 21:54:30 +0100

    Update Viewer.js

--- Viewer.js
+++ Viewer.js
@@ -1,14 +1,15 @@
 import View3D from "@egjs/view3d";
 import "@egjs/view3d/css/view3d-bundle.min.css";
 
-const view3D = new View3D("#view3d", {
-  src: "https://github.com/KhronosGroup/glTF-Sample-Models/blob/main/2.0/ToyCar/glTF/ToyCar.gltf",
-  envmap: "https://github.com/KhronosGroup/glTF-Sample-Models/blob/main/2.0/ToyCar/glTF/ToyCar_basecolor.png",
-});
+
 
 
 function Start() 
 {
+    const view3D = new View3D("#view3d", {
+    src: "https://github.com/KhronosGroup/glTF-Sample-Models/blob/main/2.0/ToyCar/glTF/ToyCar.gltf",
+    envmap: "https://github.com/KhronosGroup/glTF-Sample-Models/blob/main/2.0/ToyCar/glTF/ToyCar_basecolor.png",
+  });
     view3D.setCamera(0, 0, 0, 0, 0, 0);
     view3D.Audio = true;
     
