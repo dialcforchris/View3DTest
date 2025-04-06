c9fd731278ed69b1597bd6bf55dc970a5a878f3b
DialC <mrchrismugridge@gmail.com>
2025-04-06 21:41:46 +0100

    audio

--- Viewer.js
+++ Viewer.js
@@ -5,3 +5,11 @@ const view3D = new View3D("#view3d", {
   src: "https://github.com/KhronosGroup/glTF-Sample-Models/blob/main/2.0/ToyCar/glTF/ToyCar.gltf",
   envmap: "https://github.com/KhronosGroup/glTF-Sample-Models/blob/main/2.0/ToyCar/glTF/ToyCar_basecolor.png",
 });
+
+
+function Start() 
+{
+    view3D.play();
+    Audio = new Audio("@Audio/3D.mp3");
+    Audio.play();
+}
