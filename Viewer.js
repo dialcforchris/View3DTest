15953e614e8b98ae36bd8e2375f9ad66ee4dae3d
DialC <mrchrismugridge@gmail.com>
2025-04-10 19:48:04 +0100

    maybe

--- Viewer.js
+++ Viewer.js
@@ -18,17 +18,18 @@ function pauseAudio() {
 
 function start() 
 {
+  
     var button = document.getElementById("button");
     button.textContent = "Stop";
     playAudio();
-    // const view3D = new View3D("#view3d", {
-    // src: "https://github.com/KhronosGroup/glTF-Sample-Models/blob/main/2.0/ToyCar/glTF/ToyCar.gltf",
-    // envmap: "https://github.com/KhronosGroup/glTF-Sample-Models/blob/main/2.0/ToyCar/glTF/ToyCar_basecolor.png",
-//  });
-  //  view3D.setCamera(0, 0, 0, 0, 0, 0);
-    //view3D.Audio = true;
+     const view3D = new View3D("#wrapper-el", {
+     src: "https://github.com/KhronosGroup/glTF-Sample-Models/blob/main/2.0/ToyCar/glTF/ToyCar.gltf",
+     envmap: "https://github.com/KhronosGroup/glTF-Sample-Models/blob/main/2.0/ToyCar/glTF/ToyCar_basecolor.png",
+  });
+    view3D.setCamera(0, 0, 0, 0, 0, 0);
+    view3D.Audio = true;
     
-    //view3D.play();
-    //let audio = new Audio("../Audio/3D.mp3");
-   // audio.play();
+    view3D.play();
+    let audio = new Audio("../Audio/3D.mp3");
+    audio.play();
 }
